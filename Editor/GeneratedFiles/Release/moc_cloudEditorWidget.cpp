39b86a51c2d98a188e99eca16f868ae18dee9c60
weibo6631352 <838524022@qq.com>
2018-11-01 14:17:20 +0800

    1

---
+++ Editor/GeneratedFiles/Release/moc_cloudEditorWidget.cpp
@@ -0,0 +1,231 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'cloudEditorWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../cloudEditorWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'cloudEditorWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CloudEditorWidget_t {
+    QByteArrayData data[31];
+    char stringdata0[334];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CloudEditorWidget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CloudEditorWidget_t qt_meta_stringdata_CloudEditorWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 17), // "CloudEditorWidget"
+QT_MOC_LITERAL(1, 18, 4), // "load"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 4), // "save"
+QT_MOC_LITERAL(4, 29, 15), // "toggleBlendMode"
+QT_MOC_LITERAL(5, 45, 4), // "view"
+QT_MOC_LITERAL(6, 50, 8), // "select1D"
+QT_MOC_LITERAL(7, 59, 8), // "select2D"
+QT_MOC_LITERAL(8, 68, 8), // "select3D"
+QT_MOC_LITERAL(9, 77, 12), // "invertSelect"
+QT_MOC_LITERAL(10, 90, 12), // "cancelSelect"
+QT_MOC_LITERAL(11, 103, 4), // "copy"
+QT_MOC_LITERAL(12, 108, 5), // "paste"
+QT_MOC_LITERAL(13, 114, 6), // "remove"
+QT_MOC_LITERAL(14, 121, 3), // "cut"
+QT_MOC_LITERAL(15, 125, 9), // "transform"
+QT_MOC_LITERAL(16, 135, 7), // "denoise"
+QT_MOC_LITERAL(17, 143, 4), // "undo"
+QT_MOC_LITERAL(18, 148, 17), // "increasePointSize"
+QT_MOC_LITERAL(19, 166, 17), // "decreasePointSize"
+QT_MOC_LITERAL(20, 184, 25), // "increaseSelectedPointSize"
+QT_MOC_LITERAL(21, 210, 25), // "decreaseSelectedPointSize"
+QT_MOC_LITERAL(22, 236, 12), // "setPointSize"
+QT_MOC_LITERAL(23, 249, 4), // "size"
+QT_MOC_LITERAL(24, 254, 20), // "setSelectedPointSize"
+QT_MOC_LITERAL(25, 275, 10), // "colorByRGB"
+QT_MOC_LITERAL(26, 286, 8), // "colorByX"
+QT_MOC_LITERAL(27, 295, 8), // "colorByY"
+QT_MOC_LITERAL(28, 304, 8), // "colorByZ"
+QT_MOC_LITERAL(29, 313, 11), // "colorByPure"
+QT_MOC_LITERAL(30, 325, 8) // "showStat"
+
+    },
+    "CloudEditorWidget\0load\0\0save\0"
+    "toggleBlendMode\0view\0select1D\0select2D\0"
+    "select3D\0invertSelect\0cancelSelect\0"
+    "copy\0paste\0remove\0cut\0transform\0denoise\0"
+    "undo\0increasePointSize\0decreasePointSize\0"
+    "increaseSelectedPointSize\0"
+    "decreaseSelectedPointSize\0setPointSize\0"
+    "size\0setSelectedPointSize\0colorByRGB\0"
+    "colorByX\0colorByY\0colorByZ\0colorByPure\0"
+    "showStat"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CloudEditorWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      28,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  154,    2, 0x0a /* Public */,
+       3,    0,  155,    2, 0x0a /* Public */,
+       4,    0,  156,    2, 0x0a /* Public */,
+       5,    0,  157,    2, 0x0a /* Public */,
+       6,    0,  158,    2, 0x0a /* Public */,
+       7,    0,  159,    2, 0x0a /* Public */,
+       8,    0,  160,    2, 0x0a /* Public */,
+       9,    0,  161,    2, 0x0a /* Public */,
+      10,    0,  162,    2, 0x0a /* Public */,
+      11,    0,  163,    2, 0x0a /* Public */,
+      12,    0,  164,    2, 0x0a /* Public */,
+      13,    0,  165,    2, 0x0a /* Public */,
+      14,    0,  166,    2, 0x0a /* Public */,
+      15,    0,  167,    2, 0x0a /* Public */,
+      16,    0,  168,    2, 0x0a /* Public */,
+      17,    0,  169,    2, 0x0a /* Public */,
+      18,    0,  170,    2, 0x0a /* Public */,
+      19,    0,  171,    2, 0x0a /* Public */,
+      20,    0,  172,    2, 0x0a /* Public */,
+      21,    0,  173,    2, 0x0a /* Public */,
+      22,    1,  174,    2, 0x0a /* Public */,
+      24,    1,  177,    2, 0x0a /* Public */,
+      25,    0,  180,    2, 0x0a /* Public */,
+      26,    0,  181,    2, 0x0a /* Public */,
+      27,    0,  182,    2, 0x0a /* Public */,
+      28,    0,  183,    2, 0x0a /* Public */,
+      29,    0,  184,    2, 0x0a /* Public */,
+      30,    0,  185,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CloudEditorWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        CloudEditorWidget *_t = static_cast<CloudEditorWidget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->load(); break;
+        case 1: _t->save(); break;
+        case 2: _t->toggleBlendMode(); break;
+        case 3: _t->view(); break;
+        case 4: _t->select1D(); break;
+        case 5: _t->select2D(); break;
+        case 6: _t->select3D(); break;
+        case 7: _t->invertSelect(); break;
+        case 8: _t->cancelSelect(); break;
+        case 9: _t->copy(); break;
+        case 10: _t->paste(); break;
+        case 11: _t->remove(); break;
+        case 12: _t->cut(); break;
+        case 13: _t->transform(); break;
+        case 14: _t->denoise(); break;
+        case 15: _t->undo(); break;
+        case 16: _t->increasePointSize(); break;
+        case 17: _t->decreasePointSize(); break;
+        case 18: _t->increaseSelectedPointSize(); break;
+        case 19: _t->decreaseSelectedPointSize(); break;
+        case 20: _t->setPointSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 21: _t->setSelectedPointSize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 22: _t->colorByRGB(); break;
+        case 23: _t->colorByX(); break;
+        case 24: _t->colorByY(); break;
+        case 25: _t->colorByZ(); break;
+        case 26: _t->colorByPure(); break;
+        case 27: _t->showStat(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject CloudEditorWidget::staticMetaObject = {
+    { &QGLWidget::staticMetaObject, qt_meta_stringdata_CloudEditorWidget.data,
+      qt_meta_data_CloudEditorWidget,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *CloudEditorWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CloudEditorWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CloudEditorWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QGLWidget::qt_metacast(_clname);
+}
+
+int CloudEditorWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGLWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 28)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 28;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 28)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 28;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
